a1fb561711c223d72f49cb29d755e86af3b80351
n0id3a <giannisstathoudakis@gmail.com>
2026-07-08 15:04:38 +0300

    .

--- Ansible/files/vault-init.sh
+++ Ansible/files/vault-init.sh
@@ -1,25 +1,33 @@
 #!/bin/sh
+set -e # Stop execution if any command fails!
 
 echo "Initializing Vault..."
 
 #Init
 vault operator init -key-shares=5 -key-threshold=3 -format=json > /tmp/cluster-keys.json
-VAULT_ROOT_TOKEN=$(jq -r '.root_token' /tmp/cluster-keys.json)
-vault operator unseal $(jq -r '.unseal_keys_b64[0]' /tmp/cluster-keys.json)
-vault operator unseal $(jq -r '.unseal_keys_b64[1]' /tmp/cluster-keys.json)
-vault operator unseal $(jq -r '.unseal_keys_b64[2]' /tmp/cluster-keys.json)
 
+VAULT_ROOT_TOKEN=$(grep '"root_token":' /tmp/cluster-keys.json | awk -F'"' '{print $4}')
+UNSEAL_KEY_1=$(grep '"unseal_keys_b64":' -A 5 /tmp/cluster-keys.json | awk -F'"' 'NR==2 {print $2}')
+UNSEAL_KEY_2=$(grep '"unseal_keys_b64":' -A 5 /tmp/cluster-keys.json | awk -F'"' 'NR==3 {print $2}')
+UNSEAL_KEY_3=$(grep '"unseal_keys_b64":' -A 5 /tmp/cluster-keys.json | awk -F'"' 'NR==4 {print $2}')
+
+echo "Unsealing Vault..."
+vault operator unseal $UNSEAL_KEY_1
+vault operator unseal $UNSEAL_KEY_2
+vault operator unseal $UNSEAL_KEY_3
+
+echo "Logging in..."
 vault login $VAULT_ROOT_TOKEN
 
 #Setup Root CA
 echo "Configuring Root CA..."
-vault secrets enable pki
+vault secrets enable pki || true # || true ignores errors if already enabled
 vault secrets tune -max-lease-ttl=87600h pki
 vault write -field=certificate pki/root/generate/internal common_name="Lab Root CA" ttl=87600h > /tmp/Lab_Root_CA.crt
 
 #Setup Intermediate CA
 echo "Configuring Intermediate CA..."
-vault secrets enable -path=pki_int pki
+vault secrets enable -path=pki_int pki || true
 vault secrets tune -max-lease-ttl=43800h pki_int
 vault write -field=csr pki_int/intermediate/generate/internal common_name="Lab Intermediate CA" > /tmp/pki_intermediate.csr
 
@@ -33,7 +41,7 @@ vault write pki_int/config/urls issuing_certificates="http://vault.vault.svc:820
 
 #Enable Kubernetes Auth
 echo "Enabling Kubernetes Auth..."
-vault auth enable kubernetes
+vault auth enable kubernetes || true
 vault write auth/kubernetes/config kubernetes_host="https://kubernetes.default.svc:443"
 
 #Configure Cert-Manager Access
@@ -46,9 +54,10 @@ vault write auth/kubernetes/role/cert-manager-role bound_service_account_names=c
 
 #Configure External Secrets Operator Access
 echo "Configuring ESO Policies..."
-vault secrets enable -path=secret kv-v2
+vault secrets enable -path=secret kv-v2 || true
 cat <<EOF > /tmp/eso-policy.hcl
 path "secret/data/*" { capabilities = ["read"] }
+path "secret/metadata/*" { capabilities = ["read", "list"] }
 EOF
 vault policy write eso-policy /tmp/eso-policy.hcl
 vault write auth/kubernetes/role/eso-role bound_service_account_names=external-secrets bound_service_account_namespaces=external-secrets policies=eso-policy ttl=24h
